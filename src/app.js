b2ae6023a0e6bb35bbbbee0f18073956bd09cb78
jyothirmai0409 <jyothirmai.kattamuri@gmail.com>
2025-05-02 18:34:17 +0530

    routes and route handler

--- src/app.js
+++ src/app.js
@@ -1,22 +1,39 @@
 const express = require("express");
 const app = express();
 
-// request handler
+app.get("/user", (req, res) => {
+  // http://localhost:8000/user?userId=1&age=10
+  console.log("query params", req.query);
 
+  res.json({
+    fname: "jyothi",
+    lname: "kattamuri",
+  });
+});
+
+// reading the dynamic routes
+
+app.get("/user/:id/:name/:age", (req, res) => {
+  // http://localhost:8000/user?userId=1&age=10
+  console.log("by id's", req.params);
+
+  res.json({
+    fname: "jyothi",
+    lname: "kattamuri",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("post user successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("delete user successfully");
+});
+
+// the below handler will handle anything that comes after /xyz, /test, /1 - so always place app.use carefully in the code either top or last
 app.use("/", (req, res) => {
-  res.send("hello just");
-});
-
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.use("/test", (req, res) => {
   res.send("hello test");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello from route hello");
 });
 
 // define port
